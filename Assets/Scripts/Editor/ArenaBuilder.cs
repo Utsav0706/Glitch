a039fba0eaac7fb0d29c79036a21418099974139
Utsav0706 <utsavjivani.07@gmail.com>
2026-07-10 15:55:22 +0100

    "Arena greybox: floor + outer walls in scene"

--- Assets/Scripts/Editor/ArenaBuilder.cs
+++ Assets/Scripts/Editor/ArenaBuilder.cs
@@ -6,8 +6,9 @@ using UnityEditor.SceneManagement;
 public static class ArenaBuilder
 {
     const string ArenaRootName = "Arena";
-    const float FloorSize = 40f;   
-    const float Thickness = 0.5f; 
+    const float FloorSize = 40f;
+    const float Thickness = 0.5f;
+    const float WallHeight = 5f;
 
     
     [MenuItem("GLITCH/Arena/1. Build Floor", priority = 1)]
@@ -34,6 +35,45 @@ public static class ArenaBuilder
         Selection.activeGameObject = floor;
         EditorSceneManager.MarkSceneDirty(floor.scene);
         Debug.Log("[ArenaBuilder] Floor built (" + FloorSize + " x " + FloorSize + " m).");
+    }
+
+    [MenuItem("GLITCH/Arena/2. Build Outer Walls", priority = 2)]
+    public static void BuildOuterWalls()
+    {
+        Transform root = GetOrCreateRoot();
+
+        Transform old = root.Find("OuterWalls");
+        if (old != null) Object.DestroyImmediate(old.gameObject);
+
+        GameObject group = new GameObject("OuterWalls");
+        group.transform.SetParent(root, false);
+        Undo.RegisterCreatedObjectUndo(group, "Build Outer Walls");
+
+        float half = FloorSize * 0.5f;
+        float offset = half - Thickness * 0.5f;
+        float y = WallHeight * 0.5f;
+
+        CreateBox("Wall_North", group.transform, new Vector3(0f, y, offset), new Vector3(FloorSize, WallHeight, Thickness));
+        CreateBox("Wall_South", group.transform, new Vector3(0f, y, -offset), new Vector3(FloorSize, WallHeight, Thickness));
+        CreateBox("Wall_East", group.transform, new Vector3(offset, y, 0f), new Vector3(Thickness, WallHeight, FloorSize));
+        CreateBox("Wall_West", group.transform, new Vector3(-offset, y, 0f), new Vector3(Thickness, WallHeight, FloorSize));
+
+        Selection.activeGameObject = group;
+        EditorSceneManager.MarkSceneDirty(group.scene);
+        Debug.Log("[ArenaBuilder] Outer walls built.");
+    }
+
+    static GameObject CreateBox(string name, Transform parent, Vector3 localPos, Vector3 localScale)
+    {
+        GameObject box = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        box.name = name;
+        box.transform.SetParent(parent, false);
+        box.transform.localPosition = localPos;
+        box.transform.localScale = localScale;
+        GameObjectUtility.SetStaticEditorFlags(box,
+            StaticEditorFlags.OccluderStatic | StaticEditorFlags.OccludeeStatic |
+            StaticEditorFlags.BatchingStatic);
+        return box;
     }
 
     static Transform GetOrCreateRoot()
